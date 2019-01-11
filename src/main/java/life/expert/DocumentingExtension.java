ac692b2a6787ff9b77905d921ccf36ba6ee63308
wilmer krisp <wilmerkrisp@yahoo.com>
2019-01-11 22:33:31 +0300

    getters setter added

--- src/main/java/life/expert/DocumentingExtension.java
+++ src/main/java/life/expert/DocumentingExtension.java
@@ -2,11 +2,6 @@ package life.expert;
 
 
 
-
-
-
-
-
 //@Header@
 //--------------------------------------------------------------------------------
 //
@@ -26,7 +21,12 @@ package life.expert;
 public class DocumentingExtension
 	{
 	
+	
+	
 	private String file;
+	
+	
+	
 	public String getFile()
 		{
 		return file;
@@ -38,6 +38,7 @@ public class DocumentingExtension
 		{
 		this.file = file;
 		}
+	
 	
 	
 	private String packages;
@@ -55,6 +56,118 @@ public class DocumentingExtension
 		{
 		this.packages = packages;
 		}
+	
+	
+	
+	public Boolean enableAllInfo = false;
+	
+	
+	
+	public Boolean getEnableAllInfo()
+		{
+		
+		return enableAllInfo;
+		}
+	
+	
+	
+	public void setEnableAllInfo( final Boolean fake )
+		{
+		
+		
+		this.enableAllInfo = true;
+		}
+	
+	
+	
+	public Boolean enableClassInfo = false;
+	
+	
+	
+	public Boolean getEnableClassInfo()
+		{
+		return enableClassInfo;
+		}
+	
+	
+	
+	public void setEnableClassInfo( final Boolean fake )
+		{
+		this.enableClassInfo = true;
+		}
+	
+	
+	
+	public Boolean enableFieldInfo = false;
+	
+	
+	
+	public Boolean getEnableFieldInfo()
+		{
+		return enableFieldInfo;
+		}
+	
+	
+	
+	public void setEnableFieldInfo( final Boolean fake )
+		{
+		this.enableFieldInfo = true;
+		}
+	
+	
+	
+	public Boolean enableMethodInfo = false;
+	
+	
+	
+	public Boolean getEnableMethodInfo()
+		{
+		return enableMethodInfo;
+		}
+	
+	
+	
+	public void setEnableMethodInfo( final Boolean fake )
+		{
+		this.enableMethodInfo = true;
+		}
+	
+	
+	
+	public Boolean ignoreFieldVisibility = false;
+	
+	
+	
+	public Boolean getIgnoreFieldVisibility()
+		{
+		return ignoreFieldVisibility;
+		}
+	
+	
+	
+	public void setIgnoreFieldVisibility( final Boolean fake )
+		{
+		this.ignoreFieldVisibility = true;
+		}
+	
+	
+	
+	public Boolean ignoreMethodVisibility = false;
+	
+	
+	
+	public Boolean getIgnoreMethodVisibility()
+		{
+		return ignoreMethodVisibility;
+		}
+	
+	
+	
+	public void setIgnoreMethodVisibility( final Boolean fake )
+		{
+		this.ignoreMethodVisibility = true;
+		}
+		
 		
 		
 	}
