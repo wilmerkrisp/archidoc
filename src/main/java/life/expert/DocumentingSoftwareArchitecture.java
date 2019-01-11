fe488f619483ef381672eaf78395c7c2c72f3275
wilmer krisp <wilmerkrisp@yahoo.com>
2019-01-11 21:21:52 +0300

    blank project

---
+++ src/main/java/life/expert/DocumentingSoftwareArchitecture.java
@@ -0,0 +1,56 @@
+package life.expert;
+
+
+
+
+
+
+
+
+
+import org.jetbrains.annotations.*;                     //@NotNull
+import com.google.errorprone.annotations.Immutable;     //@Immutable
+
+import com.google.common.flogger.FluentLogger;          //log
+
+import static java.text.MessageFormat.format;           //format string
+
+import java.util.ResourceBundle;
+
+import com.google.common.collect.*;                     //ImmutableList
+
+import static com.google.common.base.Preconditions.*;   //checkArgument
+import static life.expert.common.base.Preconditions.*;  //checkCollection
+import static org.apache.commons.lang3.Validate.*;      //notEmpty(collection)
+import static life.expert.common.base.Objects.*;        //deepCopyOfObject
+
+import java.util.function.*;                            //producer supplier
+
+import static cyclops.function.Memoize.*;               //memoizeSupplier
+import static java.util.stream.Collectors.*;            //toList streamAPI
+import static java.util.function.Predicate.*;           //isEqual streamAPI
+
+
+
+//@Header@
+//--------------------------------------------------------------------------------
+//
+//                          archidoc  life.expert
+//                           wilmer 2019/01/10
+//
+//--------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+public class DocumentingSoftwareArchitecture
+	{
+	
+	
+	
+	}
